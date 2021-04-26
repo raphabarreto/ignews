9399feddd70c2d5be072ace011a345f6c4405699
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-26 12:41:06 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { stripe } from '../services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IkVFRLnWoAvsSfAsKrhwQGR');
 
   const product = {
@@ -53,5 +53,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
