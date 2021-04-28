fbb79ac736843e3303f6f0a04aafa20cef863f18
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-28 11:56:07 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,10 +2,22 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -14,36 +26,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>28 de abril de 2021</time>
-            <strong>Creating a monorepo</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-              saepe culpa delectus debitis doloremque fugiat tempore cupiditate
-              aspernatur laboriosam perspiciatis unde quo consectetur tenetur
-              corporis recusandae nisi, labore beatae. Ad!
-            </p>
-          </a>
-          <a href="#">
-            <time>28 de abril de 2021</time>
-            <strong>Creating a monorepo</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-              saepe culpa delectus debitis doloremque fugiat tempore cupiditate
-              aspernatur laboriosam perspiciatis unde quo consectetur tenetur
-              corporis recusandae nisi, labore beatae. Ad!
-            </p>
-          </a>
-          <a href="">
-            <time>28 de abril de 2021</time>
-            <strong>Creating a monorepo</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-              saepe culpa delectus debitis doloremque fugiat tempore cupiditate
-              aspernatur laboriosam perspiciatis unde quo consectetur tenetur
-              corporis recusandae nisi, labore beatae. Ad!
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -58,9 +47,25 @@ export const getStaticProps: GetStaticProps = async () => {
     { fetch: ['publication.title', 'publication.content'], pageSize: 100 }
   );
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === 'paragraph')
+          ?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: { posts },
   };
 };
