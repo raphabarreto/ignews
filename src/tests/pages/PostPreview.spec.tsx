53b0f2f65ce4b3c3834809f9d2b04ee23aab66de
Raphael Barreto <tnt.raphael@gmail.com>
2021-05-23 01:31:45 -0300

    Testando página de preview

---
+++ src/tests/pages/PostPreview.spec.tsx
@@ -0,0 +1,83 @@
+import { render, screen } from '@testing-library/react';
+import { useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { mocked } from 'ts-jest/utils';
+import Post, { getStaticProps } from '../../pages/posts/preview/[slug]';
+import { getPrismicClient } from '../../services/prismic';
+
+const post = {
+  slug: 'my-new-post',
+  title: 'My new Post',
+  content: '<p>Post content</p>',
+  updatedAt: '23 de maio de 2021',
+};
+
+jest.mock('next-auth/client');
+jest.mock('next/router');
+jest.mock('../../services/prismic');
+
+describe('Post preview page', () => {
+  it('renders correctly', () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<Post post={post} />);
+
+    expect(screen.getByText('My new Post')).toBeInTheDocument();
+    expect(screen.getByText('Post content')).toBeInTheDocument();
+    expect(screen.getByText('Wanna continue reading?')).toBeInTheDocument();
+  });
+
+  it('redirects user to full post when user is subscribed', async () => {
+    const useSessionMocked = mocked(useSession);
+    const userRouterMocked = mocked(useRouter);
+    const pushMock = jest.fn();
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        activeSubscription: 'fake-active-subscription',
+      },
+      false,
+    ] as any);
+
+    userRouterMocked.mockReturnValueOnce({
+      push: pushMock,
+    } as any);
+
+    render(<Post post={post} />);
+
+    expect(pushMock).toHaveBeenCalledWith('/posts/my-new-post');
+  });
+
+  it('loads initial data', async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient);
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [{ type: 'heading', text: 'My new Post' }],
+          content: [{ type: 'paragraph', text: 'Post content' }],
+        },
+        last_publication_date: '05-23-2021',
+      }),
+    } as any);
+
+    const response = await getStaticProps({
+      params: { slug: 'my-new-post' },
+    } as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: 'my-new-post',
+            title: 'My new Post',
+            content: '<p>Post content</p>',
+            updatedAt: '23 de maio de 2021',
+          },
+        },
+      })
+    );
+  });
+});
